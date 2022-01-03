5b5639ebd21d0eb1d0e7b681f1abfa1e7c4a6731
Salar <mhmdi.salar@gmail.com>
2022-01-04 01:03:50 +0330

    Update vncfix.sh

--- vnc/vncfix.sh
+++ vnc/vncfix.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 pacman -S --needed tar wget sed --noconfirm
-pacman -U https://raw.githubusercontent.com/blanckth/blanckarm/master/vnc/tigervnc-1.10.1-1-aarch64.pkg.tar.xz --noconfirm
-curl -s https://raw.githubusercontent.com/blanckth/blanckarm/master/vnc/lib.tar.xz -o /usr/lib/a.tar.xz && tar xf /usr/lib/a.tar.xz -C /usr/lib
+pacman -U https://raw.githubusercontent.com/blanckth/blanckarm/main/vnc/tigervnc-1.10.1-1-aarch64.pkg.tar.xz --noconfirm
+curl -s https://raw.githubusercontent.com/blanckth/blanckarm/main/vnc/lib.tar.xz -o /usr/lib/a.tar.xz && tar xf /usr/lib/a.tar.xz -C /usr/lib
 sed -i '27i IgnorePkg = tigervnc' /etc/pacman.conf
