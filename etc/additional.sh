40e70e41e5c9e1423d48b1abd77162a95aeb7072
Salar <mhmdi.salar@gmail.com>
2022-01-04 00:44:05 +0330

    Update additional.sh

--- etc/additional.sh
+++ etc/additional.sh
@@ -3,13 +3,14 @@
 ## Authur Salar Muhammadi
 #####################################################
 systemctl disable systemd-resolved.service;
+echo 'export LC_ALL=en_US.UTF-8"' >> /etc/profile;
+source /etc/profile;
 rm /etc/resolv.conf;
 mv resolv.conf /etc/;
 pacman-key --init;
 echo "disablescdaemon" > /etc/pacman.d/gnupg/gpg-agent.conf;
 pacman-key --populate archlinuxarm;
 chmod 755 /bin /home /mnt /run /srv /tmp /var /boot /etc /opt /root /sbin /sys /usr;
-LC_ALL=C;
 pacman -Syu --needed --noconfirm;
 pacman -S --needed wget sudo --noconfirm;
 #####################################################
